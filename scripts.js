7f84ba92ff7ef484729f4f144d19ce2d5638225d
tomasmaillo <tomasroma64@gmail.com>
2020-06-16 23:28:22 +0200

    Basic detailed view on click

--- scripts.js
+++ scripts.js
@@ -1,21 +1,50 @@
 $(document).ready(function() {
 
-
     $.get("https://api.spacexdata.com/v3/launches/upcoming", function(launches_data) {
-        //$('#launches').text(launches_data);
         console.log(launches_data);
 
-        var tr;
-        for (var i = 0; i < launches_data.length; i++) {
-            tr = $('<tr/>');
+
+        for (let i = 0; i < launches_data.length; i++) {
+            let tr;
+            let tr_id = "record_" + i;
+            let detailed_id = "detailed_" + i;
+
+            tr = $('<tr id=\"' + tr_id + '\">');
             tr.append("<td>" + launches_data[i].flight_number + "</td>");
-            tr.append("<td>" + launches_data[i].details + "</td>");
+            tr.append("<td>" + launches_data[i].launch_date_utc + "</td>");
+            tr.append("<td>" + launches_data[i].mission_name + "</td>");
             tr.append("<td>" + launches_data[i].rocket.rocket_id + "</td>");
+            tr.append('</tr>');
             $('#launches').append(tr);
+
+
+            $('#' + tr_id).on('click', function() {
+                $('.launchDetail').hide();
+                $('#' + detailed_id).show();
+            });
+
+            detailed = $('<div class=\"launchDetail\" id=\"' + detailed_id + '\">');
+
+            detailed.append($('<p>' + 'Date' + '</p>'));
+            detailed.append($('<p>' + launches_data[i].launch_date_utc + '</p>'));
+            detailed.append($('<br>'));
+
+            detailed.append($('<p>' + 'Mission name' + '</p>'));
+            detailed.append($('<p>' + launches_data[i].mission_name + '</p>'));
+            detailed.append($('<br>'));
+
+            detailed.append($('<p>' + 'Flight number' + '</p>'));
+            detailed.append($('<p>' + launches_data[i].flight_number + '</p>'));
+            detailed.append($('<br>'));
+
+            detailed.append($('<p>' + 'Details' + '</p>'));
+            detailed.append($('<p>' + launches_data[i].details + '</p>'));
+            detailed.append('</div>');
+
+            $('#detailed').append(detailed);
+            $('#' + detailed_id).hide();
+
         }
 
-
     });
-
-
 });
